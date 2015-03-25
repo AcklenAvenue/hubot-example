7d2d52d36740b7898d862b5d565115199d585db8
Byron Sommardahl <byron@acklenavenue.com>
2015-03-25 15:34:17 -0500

    Cleaning up.

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,12 @@ var gulp = require('gulp');
 var clean = require('gulp-clean');
 var debug = require('gulp-debug');
 var istanbul = require('gulp-istanbul');
+var buildFolder = 'build';
 
 gulp.task('default', ['build', 'specs', 'deploy']);
 
 gulp.task('build', ['clean'], function(){
-	console.log("Compiling TypeScript files from the /src folder and placing new JavaScript files in the /dist folder.")		
+	console.log("Compiling TypeScript files from the /src folder and placing new JavaScript files in the '"+buildFolder+"' folder.")		
 	var tsc  = require('gulp-typescript')	
 	return gulp.src(["typings/**/*.ts", "src/**/*.ts"])		
 		.pipe(tsc({
@@ -16,34 +17,34 @@ gulp.task('build', ['clean'], function(){
 		            declarationFiles: true,
 		            emitError: false,
 		            comments: true
-		       }))
-		.pipe(gulp.dest('dist'));
+		            }))		       
+		.pipe(gulp.dest(buildFolder));
 });
 
 gulp.task('clean', function () {
 	console.log("Giving the build script a clean slate.")	
-	return gulp.src(['dist', 'scripts'], {read: false})
+	return gulp.src([buildFolder, 'scripts'], {read: false})
         .pipe(clean());
 });
 
 gulp.task('specs', ['build'], function(cb){
 	console.log("Running specs to make sure nothing is broken.")
 	var mocha = require('gulp-mocha');	
-	gulp.src(["dist/**/*.js", "!dist/specs/**/*.js"])
+	gulp.src([buildFolder+"/**/*.js", "!"+buildFolder+"/specs/**/*.js"])
 		.pipe(istanbul())
 		.pipe(istanbul.hookRequire())
 		.on('finish', function () {
-	      gulp.src(['dist/specs/**/*.js'])
+	      gulp.src([buildFolder+'/specs/**/*.js'])
 	        .pipe(mocha())
-	        .pipe(istanbul.writeReports({reporters: ['text-summary','html'], reportOpts: { dir: "reports"}}))
+	        .pipe(istanbul.writeReports({reporters: ['text-summary','html'], reportOpts: { dir: "coverage"}}))
 	        .on('end', cb);
 	    }); 
 });
 
 gulp.task('deploy', ['build', 'specs'], function(){
 	console.log("Deploying the hubot script files to the /scripts folder so that hubot will include them.")
-	gulp.src('dist/scripts/*.js')
+	gulp.src(buildFolder+'/scripts/*.js')
 		.pipe(gulp.dest('scripts'));
-	gulp.src('dist/helpers/*.js')
+	gulp.src(buildFolder+'/helpers/*.js')
 		.pipe(gulp.dest('helpers'));
 });
